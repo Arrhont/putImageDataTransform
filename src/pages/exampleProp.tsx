c5a1145d6ecbba6195223453764acf5c8ba40f14
evgender <evgender@yandex-team.ru>
2021-09-30 23:13:06 +0300

    Pash ti pizdec kanesh...

--- src/pages/exampleProp.tsx
+++ src/pages/exampleProp.tsx
@@ -1,17 +1,41 @@
-import React, { useEffect, useRef } from "react";
-import { createNoSubstitutionTemplateLiteral } from "typescript";
+import React, {useEffect, useRef } from "react";
+
+const getAreaImage = (imageData: ImageData) => {
+  const upperLeftAngle = { x: imageData.width, y: imageData.height };
+  const bottomRightAngle = { x: 0, y: 0 };
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    if (imageData.data[i + 3]) {
+      const pixelNumber = Math.floor(i / 4);
+      const x = pixelNumber % imageData.width + 1;
+      const y = Math.floor(pixelNumber / imageData.width) + 1;
+
+      if (x < upperLeftAngle.x) {
+        upperLeftAngle.x = x;
+      }
+      if (y < upperLeftAngle.y) {
+        upperLeftAngle.y = y;
+      }
+      if (x > bottomRightAngle.x) {
+        bottomRightAngle.x = x;
+      }
+      if (y > bottomRightAngle.y) {
+        bottomRightAngle.y = y;
+      }
+    }
+  }
+
+  const width = bottomRightAngle.x - upperLeftAngle.x;
+  const height = bottomRightAngle.y - upperLeftAngle.y;
+
+  return [upperLeftAngle.x, upperLeftAngle.y, width, height] as const;
+};
 
 export default function ExampleProp({
   markers,
-}: {
-  markers: { name: string; language: string }[];
+}: { markers: { name: string; language: string }[];
 }) {
-  // const p: JSX.Element[][] = markers.map((marker) =>
-  //   Object.values(marker).map((value) => <div>{value}</div>)
-  // );
-
   const sourceCanvasElementRef = useRef<HTMLCanvasElement>(null);
-  const sourceCanvasElementRef2 = useRef<HTMLCanvasElement>(null);
   const targetCanvasElementRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -39,7 +63,7 @@ export default function ExampleProp({
       sourceContext.fillRect(270, 130, 100, 100);
 
       sourceContext.fillStyle = "red";
-      sourceContext.fillRect(380, 130, 100, 100);
+      sourceContext.fillRect(380, 20, 100, 100);
 
       sourceContext.fillStyle = "green";
       sourceContext.fillRect(45, 130, 100, 100);
@@ -53,18 +77,13 @@ export default function ExampleProp({
       const contextFullImageData = sourceContext?.getImageData(
         0,
         0,
-        sourceCanvasElementRef.current.offsetWidth,
-        sourceCanvasElementRef.current.offsetHeight
+        sourceCanvasElementRef.current.width,
+        sourceCanvasElementRef.current.height
       );
 
       const areaImage = getAreaImage(contextFullImageData);
 
-      const imageData = sourceContext?.getImageData(
-        areaImage.minX,
-        areaImage.minY,
-        areaImage.maxX,
-        areaImage.maxY
-      );
+      const imageData = sourceContext?.getImageData(...areaImage);
 
       if (targetContext) {
         const proxyCanvas = document.createElement("canvas");
@@ -72,97 +91,37 @@ export default function ExampleProp({
         proxyCanvas.height = imageData.height;
         proxyCanvas.getContext("2d")?.putImageData(imageData, 0, 0);
 
-        const scaleX = imageData.width / targetCanvasElementRef.current.width;
-        const scaleY = imageData.height / targetCanvasElementRef.current.height;
-        const scale = Math.min(scaleX, scaleY);
+        const imageDataAspectRatio = imageData.width / imageData.height;
+        const targetAspectRatio = targetContext.canvas.width / targetContext.canvas.height;
 
-        console.log(imageData);
+        const ratioFactor = imageDataAspectRatio / targetAspectRatio
 
         targetContext.drawImage(
           proxyCanvas,
           0,
           0,
-          targetCanvasElementRef.current.width,
-          targetCanvasElementRef.current.height
+            ratioFactor > 1 ? targetContext.canvas.width : targetContext.canvas.width * ratioFactor,
+            ratioFactor > 1 ? targetContext.canvas.height * ratioFactor : targetContext.canvas.height,
         );
       }
     }
   }, []);
 
-  const getAreaImage = (imageData: ImageData) => {
-    let curPixX = 0;
-    let curPixY = 0;
-    const upperLeftAngle = { x: imageData.width, y: imageData.height };
-    const bottomRightAngle = { x: 0, y: 0 };
-    for (let i = 0; i < imageData.data.length; i++) {
-      if (i % 4 === 0) {
-        curPixX++;
-      }
-      if (imageData.data[i + 4] && curPixX < upperLeftAngle.x) {
-        upperLeftAngle.x = curPixX;
-      }
-      if (imageData.data[i + 4] && curPixY < upperLeftAngle.y) {
-        upperLeftAngle.y = curPixY;
-      }
-      if (imageData.data[i + 4] && curPixX > bottomRightAngle.x) {
-        bottomRightAngle.x = curPixX;
-      }
-      if (imageData.data[i + 4] && curPixY > bottomRightAngle.y) {
-        bottomRightAngle.y = curPixY;
-      }
-      if (curPixX === imageData.width) {
-        curPixY++;
-        curPixX = curPixY === imageData.height ? curPixX : 0;
-      }
-    }
-
-    return {
-      minX: upperLeftAngle.x,
-      minY: upperLeftAngle.y,
-      maxX: bottomRightAngle.x,
-      maxY: bottomRightAngle.y,
-    };
-  };
-
   return (
-    <>
-      <div
-        style={{
-          padding: "1rem",
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
-          width: "600px",
-        }}
-      >
+    <div>
+
         <canvas
-          style={{ border: "1px solid black" }}
-          ref={sourceCanvasElementRef}
-          width="600"
-          height="700"
-        ></canvas>
+    style={{border: "1px solid black"}}
+    ref={sourceCanvasElementRef}
+    width="600"
+    height="700"
+    />
         <canvas
-          style={{ border: "1px solid black" }}
-          ref={sourceCanvasElementRef2}
-          width="600"
-          height="700"
-        ></canvas>
-      </div>
-      <div
-        style={{
-          padding: "1rem",
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
-          width: "120px",
-        }}
-      >
-        <canvas
-          style={{ border: "1px solid black" }}
-          ref={targetCanvasElementRef}
-          height="120"
-        ></canvas>
-      </div>
-    </>
+    style={{border: "1px solid black"}}
+    ref={targetCanvasElementRef}
+    width="200px"
+    height="120px"
+    />
+    </div>
   );
 }
